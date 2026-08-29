ed7eab3718377ab23d7352691be570c9507fd649
felipekenzookita <felipekenzookita@gmail.com>
2026-08-29 15:00:25 -0300

    Add files via upload

---
+++ cyberchase/13.sql
@@ -0,0 +1,1 @@
+SELECT "title", "air_date" FROM "episodes" WHERE "season" = 1 AND "topic" IS NOT NULL;
---
+++ players/10.sql
@@ -0,0 +1,1 @@
+SELECT "first_name" FROM "players" WHERE "birth_country" = 'USA' ORDER BY "weight" DESC LIMIT 10;
